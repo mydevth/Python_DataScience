a22a31ecf539ea17d267dad4b65f798419efb0e6
mydevth <mydevth@chonburi.biz>
2022-06-05 20:44:47 +0700

    first

---
+++ firstcode.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a = 12
+b = a*4
+print(b)
+
+
+# In[6]:
+
+
+name='akkadate'
+print(name,b)
+
+
+# In[5]:
+
+
+b = 512
+
+
+# In[7]:
+
+
+get_ipython().run_line_magic('pinfo', 'plt.bar')
+
+
+# In[8]:
+
+
+get_ipython().run_line_magic('pinfo', 'plt.bar')
+
+
+
+# In[9]:
+
+
+import matplotlib_inline as mp
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### dfdfdfdfdfdfdfsdfs
+# 
+
+# In[ ]:
+
+
+
+
